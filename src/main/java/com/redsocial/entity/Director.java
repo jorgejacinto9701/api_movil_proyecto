7ce820e9d55baafe4611d7b79c5aebfca1adeb0b
jorgejacinto9701 <jorgejacinto9701@gmail.com>
2020-11-09 16:17:30 -0500

    Cambios

--- src/main/java/com/redsocial/entity/Director.java
+++ src/main/java/com/redsocial/entity/Director.java
@@ -1,7 +1,5 @@
 package com.redsocial.entity;
 
-import java.util.Date;
-
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
@@ -10,10 +8,6 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
-
-import org.springframework.format.annotation.DateTimeFormat;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
@@ -30,11 +24,7 @@ public class Director {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int idDirector;
 	private String nombre;
-	
-	
-	@Temporal(TemporalType.DATE)
-	@DateTimeFormat(pattern = "yyyy-MM-dd")
-	private Date fechaNacimiento;
+	private String apellido;
 	
 	@JsonIgnore
 	@ManyToOne(fetch = FetchType.LAZY)
