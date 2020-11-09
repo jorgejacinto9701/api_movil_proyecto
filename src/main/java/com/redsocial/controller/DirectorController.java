8eb6d5f9b5bc464f39aedee785060e2e92ef8b5b
jorgejacinto9701 <jorgejacinto9701@gmail.com>
2020-11-09 16:38:56 -0500

    Nuevos datos

--- src/main/java/com/redsocial/controller/DirectorController.java
+++ src/main/java/com/redsocial/controller/DirectorController.java
@@ -1,7 +1,10 @@
 package com.redsocial.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,7 +25,10 @@ public class DirectorController {
 		return ResponseEntity.ok(service.inserta(obj));
 	}
 	
-	
+	@GetMapping
+	public ResponseEntity<List<Director>> lista() {
+		return ResponseEntity.ok(service.listaDirector());
+	}
 	
 }
 
--- src/main/java/com/redsocial/service/DirectorServiceImpl.java
+++ src/main/java/com/redsocial/service/DirectorServiceImpl.java
@@ -1,5 +1,7 @@
 package com.redsocial.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,6 +18,12 @@ public class DirectorServiceImpl implements DirectorService{
 	@Override
 	public Director inserta(Director obj) {
 		return repository.save(obj);
+	}
+
+
+	@Override
+	public List<Director> listaDirector() {
+		return repository.findAll();
 	}
 
 }
